c148d6c1f6850c1f6de670b34435700403909acc
Ben Whittle <benwhittle31@gmail.com>
2018-02-01 11:50:13 -0800

    using new tensorci-client XYScatterPlot

--- src/model.py
+++ src/model.py
@@ -1,12 +1,25 @@
 from definitions import model_path
 from time import sleep
+from tensorci_client.graphs.xy_scatter_plot import XYScatterPlot
+
+graph = XYScatterPlot(title='Loss vs. Iterations',
+                      x_axis='Iterations',
+                      y_axis='Loss')
 
 
 def train():
   print('Training...')
 
+  train_series = graph.series(name='Train Series', color='#ff7277')
+
   for i in range(25):
-    print('Epoch {}...'.format(i))
+    print('Iter {}...'.format(i))
+
+    # Get training loss
+    loss = 1
+
+    train_series.add_data_point(x=i, y=loss)
+
     i += 1
     sleep(1)
 
@@ -20,9 +33,23 @@ def train():
   sleep(1)
 
 
-
 def test():
   print('Testing...')
+
+  test_series = graph.series(name='Test Series', color='#35ACC4')
+
+  for i in range(25):
+    print('Iter {}...'.format(i))
+
+    # Get test loss
+    loss = 1
+
+    test_series.add_data_point(x=i, y=loss)
+
+    i += 1
+    sleep(1)
+
+  print('Done testing.')
 
 
 def predict(data):
