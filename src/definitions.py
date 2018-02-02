44e3e70f80752cc688368334ae3acffc56772ffc
Ben Whittle <benwhittle31@gmail.com>
2018-02-01 16:25:07 -0800

    importing compute_train_loss and compute_test_loss methods

--- src/definitions.py
+++ src/definitions.py
@@ -5,3 +5,6 @@ base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 data_dir = base_dir + '/data'
 
 model_path = data_dir + '/model.ckpt'
+
+train_steps = 25
+test_steps = 25
--- src/model.py
+++ src/model.py
@@ -1,67 +1,70 @@
-from definitions import model_path
-from time import sleep
 import os
+from time import sleep
+from definitions import model_path, train_steps, test_steps
+from utils import compute_train_loss, compute_test_loss
 from tensorci_client.graphs.xy_scatter_plot import XYScatterPlot
 
+# Create TensorCI scatter plot to log real-time loss info to
 graph = XYScatterPlot(title='Loss vs. Iterations',
                       x_axis='Iterations',
                       y_axis='Loss')
 
+
 def train():
-  print('Training...')
+  print('Training model...')
 
-  print('Graph: {}'.format(graph))
-
-  if graph:
-    print('Graph DICT: {}'.format(str(graph.__dict__)))
-
-  print('REDIS_URL: {}'.format(os.environ.get('REDIS_URL')))
-  print('TENSORCI_TRAIN_SECRET: {}'.format(os.environ.get('TENSORCI_TRAIN_SECRET')))
-
+  # Create Train data series for the scatter plot
   train_series = graph.series(name='Train Series', color='#ff7277')
 
-  for i in range(25):
-    print('Iter {}...'.format(i))
+  for i in range(train_steps):
+    print('Train iteration {}/{}'.format(i + 1, train_steps))
 
-    # Get training loss
-    loss = 1
+    # Find training loss
+    loss = compute_train_loss(i)
 
+    # Add this loss to our graph
     train_series.add_data_point(x=i, y=loss)
 
-    i += 1
     sleep(1)
 
-  print('Done training.')
+  print('Training complete.')
 
+  # Save the trained model
+  save_model()
+
+
+def test():
+  print('Testing model accuracy...')
+
+  # Create Test data series for the scatter plot
+  test_series = graph.series(name='Test Series', color='#35ACC4')
+
+  for i in range(test_steps):
+    print('Test iteration {}/{}'.format(i + 1, test_steps))
+
+    # Find test loss
+    loss = compute_test_loss(i)
+
+    # Add this loss to our graph
+    test_series.add_data_point(x=i, y=loss)
+
+    sleep(1)
+
+  print('Testing complete.')
+
+
+def predict(data):
+  return 'i-love-TensorCI'
+
+
+def save_model():
   # Training will produce the model file
   with open(model_path, 'w+') as f:
     f.write('I love TensorCI!')
 
   print('Writing model file...')
+
   sleep(1)
-
-
-def test():
-  print('Testing...')
-
-  test_series = graph.series(name='Test Series', color='#35ACC4')
-
-  for i in range(25):
-    print('Iter {}...'.format(i))
-
-    # Get test loss
-    loss = 1
-
-    test_series.add_data_point(x=i, y=loss)
-
-    i += 1
-    sleep(1)
-
-  print('Done testing.')
-
-
-def predict(data):
-  return 'i-love-TensorCI'
 
 
 def reload():
